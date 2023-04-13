ec14c221e1e6811d96352548e6825951e65f2d80
CChristofi1 <chambakis.cc@gmail.com>
2023-04-13 20:40:26 +0300

    User-API-Refactor Task - Charalambos Christofi

--- Tests.User.Api/Tests.User.Api/Models/User.cs
+++ Tests.User.Api/Tests.User.Api/Models/User.cs
@@ -1,13 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
-
+using System.ComponentModel.DataAnnotations;
+/// Added the library in order to make a parameter be required
 namespace Tests.User.Api.Models
 {
     [Table("users")]
     public class User
     {
+
+
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Age { get; set; }
     }
 }
