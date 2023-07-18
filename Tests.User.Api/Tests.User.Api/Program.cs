0dc55149fa50454fc97c6c19c9d998b92e924d6f
JennCarter <46348721+JennCarter@users.noreply.github.com>
2023-07-18 17:20:50 +0100

    Update Program.cs
    Add db context to container

--- Tests.User.Api/Tests.User.Api/Program.cs
+++ Tests.User.Api/Tests.User.Api/Program.cs
@@ -1,8 +1,14 @@
+// register DB context
+using Microsoft.EntityFrameworkCore;
+using Tests.User.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Add db context to container using in-memory database
+builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("DefaultConnection"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
