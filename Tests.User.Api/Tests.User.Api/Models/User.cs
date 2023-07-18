fb6d6d260521cc4dddb60476a10615313ce0bc80
JennCarter <46348721+JennCarter@users.noreply.github.com>
2023-07-18 16:56:09 +0100

    Update User.cs

--- Tests.User.Api/Tests.User.Api/Models/User.cs
+++ Tests.User.Api/Tests.User.Api/Models/User.cs
@@ -6,8 +6,16 @@ namespace Tests.User.Api.Models
     public class User
     {
         public int Id { get; set; }
+       
+        // Validation for first name, last name and age - a user is required to have these
+        [Required("First name is required")]
         public string FirstName { get; set; }
+        
+        [Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; }
+
+        // Age is changed to an integer data type
+        [Required(ErrorMessage = "Age is required")]
         public int Age { get; set; }
     }
 }
