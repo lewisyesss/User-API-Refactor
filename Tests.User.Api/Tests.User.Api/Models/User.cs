8501664687aea95a6ed9ae2403e4b64880566b75
Ben Dingwall <paleeeee@outlook.com>
2023-08-03 23:29:54 +0100

    User Controller - Using dependency injection rather than creating a new instance within each method. Updated each method to be async for any interaction with the database. Null checks on Get/Update/Delete if the user doesn't exist.
    User / Database Context -
    Allow the use of nullable values.
    
    UserControllerTests -
    Becasue we're now using dependency injection within the UserController, the constructor needs to be passed a DatabaseContext. To do that, we're wrapping each body inside a using block to instantiate our DatabaseContext.

--- Tests.User.Api/Tests.User.Api/Models/User.cs
+++ Tests.User.Api/Tests.User.Api/Models/User.cs
@@ -6,8 +6,8 @@ namespace Tests.User.Api.Models
     public class User
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Age { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Age { get; set; }
     }
 }
